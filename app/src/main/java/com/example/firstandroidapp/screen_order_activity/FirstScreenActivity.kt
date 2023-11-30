b07efc3582d1d20db3c1de46c1afbb5ff2c59dbc
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-11-30 19:45:46 +0100

    finished screen orders

--- app/src/main/java/com/example/firstandroidapp/screen_order_activity/FirstScreenActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/screen_order_activity/FirstScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstandroidapp.screen_order_activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -9,8 +10,25 @@ import com.example.firstandroidapp.R
 class FirstScreenActivity : AppCompatActivity() {
     private lateinit var etName : EditText
     private lateinit var btnGoToNextScreen : AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_screen)
+        findViews()
+        onClick()
+    }
+
+    private fun onClick() {
+        btnGoToNextScreen.setOnClickListener {
+            val inputValue = etName.text.toString()
+            val intent = Intent(this@FirstScreenActivity,SecondScreenActivity::class.java)
+            intent.putExtra("etName",inputValue)
+            startActivity(intent)
+        }
+    }
+
+    private fun findViews() {
+        btnGoToNextScreen = findViewById(R.id.btnGoToNextScreen)
+        etName = findViewById(R.id.etName)
     }
 }
--- app/src/main/java/com/example/firstandroidapp/screen_order_activity/SecondScreenActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/screen_order_activity/SecondScreenActivity.kt
@@ -2,11 +2,16 @@ package com.example.firstandroidapp.screen_order_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.firstandroidapp.R
 
 class SecondScreenActivity : AppCompatActivity() {
+    private lateinit var tvShowDataFromFirstScreen : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_screen)
+        tvShowDataFromFirstScreen = findViewById(R.id.tvShowDataFromFirstScreen)
+        val name = intent.getStringExtra("etName")
+        tvShowDataFromFirstScreen.text = name
     }
 }
